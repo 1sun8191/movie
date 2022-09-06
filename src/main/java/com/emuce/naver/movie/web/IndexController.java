77aad111e29228dd8d787cb8d00dad7a8959e9fb
1sun0901 <emuce@naver.com>
2022-09-06 23:40:05 +0900

    mustache 일부 수정

--- src/main/java/com/emuce/naver/movie/web/IndexController.java
+++ src/main/java/com/emuce/naver/movie/web/IndexController.java
@@ -1,15 +1,37 @@
 package com.emuce.naver.movie.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("/posts/write")
+    public String postsWrite() {
+        return "posts-write";
+    }
+
+    @GetMapping("/posts/future")
+    public String postsFuture() {
+        return "posts-future";
+    }
+
+    @GetMapping("/posts/score")
+    public String postsScore() {
+        return "posts-score";
+    }
+
+    @GetMapping("/posts/review")
+    public String postsSave() {
+        return "posts-review";
+    }
+
+
 
 
 
